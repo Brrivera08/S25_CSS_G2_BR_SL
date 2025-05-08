65c4ba9eb508e455cfcc377823b0c0e05c37322a
aamata1 <aamata1@student.uiwtx.edu>
2025-05-07 19:27:16 -0500

    issue 5 bug fix

--- Issue-5/app copy.py
+++ Issue-5/app copy.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, session
-import os, random, datetime
+import os, random, datetime, timedelta
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
